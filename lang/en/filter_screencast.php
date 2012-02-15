fb4ee52d210a868ec8b3e0504132afdc265aa23f
Mark Schall <M.Schall@TechSmith.com>
2012-02-15 14:06:07 -0500

    Adding copyright and licensing info to all the source files

--- lang/en/filter_screencast.php
+++ lang/en/filter_screencast.php
@@ -1,12 +1,12 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle-Screencast-Filter
 //
-// Moodle is free software: you can redistribute it and/or modify
+// Moodle-Screencast-Filter is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// Moodle-Screencast-Filter is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -15,10 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'filter_mediaplugin', language 'en', branch 'MOODLE_20_STABLE'
+ * Strings for component 'filter_screencast', language 'en'
  *
- * @package   filter_mediaplugin
- * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @package   filter_screencast
+ * @copyright 2012 Mark Schall, TechSmith Corporation (link: http://techsmith.com)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
